3a78c2729b1785d0fd4ada400e5dac432d3426aa
Todd Birchard <todd.birchard@wework.com>
2018-10-30 17:14:08 -0400

    yaml

--- app.py
+++ app.py
@@ -72,3 +72,10 @@ def notebook():
                         }
             result = col.replace_one({'url': document['url']}, document, upsert=True)
             return render_template('/notebook.html', content=extract, template="notebook-template")
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
