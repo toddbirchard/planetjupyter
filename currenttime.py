ba7a38b072781274ab3bfd2f995293a3a0994b74
Todd Birchard <toddbirchard@gmail.com>
2019-01-10 20:26:56 -0500

    tweaking for no reason

--- currenttime.py
+++ currenttime.py
@@ -1,18 +1,15 @@
 from datetime import datetime
 
-
 def getTime():
     """Get user's current time."""
     rightnow = datetime.today()
     return rightnow
-
 
 def getPrettyTime():
     """Get user's pretty current time."""
     rightnow = datetime.today()
     prettytime = rightnow.ctime()
     return prettytime
-
 
 yourtime = getTime()
 prettytime = getPrettyTime()
